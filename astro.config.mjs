17e496a8889fd21272e8635ce5245de38e04b7ab
SoWiEee <kawiisowieee@gmail.com>
2025-07-06 15:05:28 +0800

    modify domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog-chg.pages.dev/",
+	site: "https://astro.mintice.blog/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
